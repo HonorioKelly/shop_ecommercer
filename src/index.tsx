60263680d84d5a143ca370ccc085460fa900d65e
HonorioKelly <kelly.pocoacahonor10@gmail.com>
2025-03-06 10:24:30 -0300

    Atualização das paginas

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,12 @@ function Home() {
   return (
 
   <div>
-    <h1></h1>
-
+    <div className="nav-menu">
+          <a href="#" className="nav-link">Shop</a>
+          <a href="#" className="nav-link">On Sale</a>
+          <a href="#" className="nav-link">New Arrivals</a>
+          <a href="#" className="nav-link">Brands</a>
+        </div>
   </div>
     
   );
