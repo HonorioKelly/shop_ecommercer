bb0d08e48583a9054320ec4ba64d783c5d6bd4f9
HonorioKelly <kelly.pocoacahonor10@gmail.com>
2025-03-05 22:14:14 -0300

    Atualização

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './pages/Homes'
+
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <div className="banner-promo">
+      <div className="banner-texto">
+        <span>Sign up and get 20% off to your first order.</span>
+        <span className="sign-up-now">Sign Up Now</span>
+    </div>
+
+      <div className="navbar">
+      <button className="menu-toggle">☰</button>
+      <b className="logo">SHOP.CO</b>
+
+      </div>
+    </div>
     <Home />
   </StrictMode>,
 )
